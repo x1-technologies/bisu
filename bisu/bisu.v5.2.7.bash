edd023ab9d1e720fa5f7c9c82629dbc8a97bcf6b
Rick Godfrey <rickgodfreygo@hotmail.com>
2025-03-16 02:26:10 +0800

    updated BISU to v5.2.7
    improvements

--- bisu/bisu.v5.2.7.bash
+++ bisu/bisu.v5.2.7.bash
@@ -5,7 +5,7 @@
 ## Have a fresh installation for BISU with copy and paste the command below
 ## sudo curl -sL https://go2.vip/bisu-file -o ./bisu.bash && sudo chmod 755 ./bisu.bash && sudo ./bisu.bash -f install
 # Define BISU VERSION
-export BISU_VERSION="5.2.6"
+export BISU_VERSION="5.2.7"
 # Minimal Bash Version
 export MINIMAL_BASH_VERSION="5.0.0"
 export _ASSOC_KEYS=()   # Core array for the common associative array keys, no modification
@@ -2269,6 +2269,7 @@ confirm_to_install() {
     local choice="y"
     local current_file=$(current_file)
     local current_filename=$(current_filename)
+    local target_path=$(target_path)
     local confirm_msg="Are you sure to install $current_filename?"
 
     [[ -z "$action" ]] && action="$option_force"
@@ -2279,10 +2280,10 @@ confirm_to_install() {
 
         if is_installed; then
             choice="n"
-            confirm_msg="$current_filename has already installed at: $current_file. Do you want to reinstall it?"
+            confirm_msg="$current_filename has already installed at: $target_path. Do you want to reinstall it?"
 
             if [[ "$force" == "false" ]]; then
-                error_exit "$current_filename has already installed at: $current_file, \
+                error_exit "$current_filename has already installed at: $target_path, \
                 please use -f if you want to forcefully override it."
             fi
         fi
