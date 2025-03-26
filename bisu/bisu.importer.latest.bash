962c4f34f8d06333307d8521d5ded41119f1c40a
Rick Godfrey <rickgodfreygo@hotmail.com>
2025-03-26 15:56:05 +0800

    upgraded BISU Importer to the v7 standard
    Upgraded BISU Importer to the v7 standard.
    Reduced complexity significantly compared to the previous version.

--- bisu/bisu.importer.latest.bash
+++ bisu/bisu.importer.latest.bash
@@ -3,25 +3,30 @@
 ## Official Web Site: https://bisu.x-1.tech
 ## Recommended BISU PATH: /usr/local/sbin/bisu.bash
 ## Have a fresh installation for BISU with copy and paste the command below
-## sudo curl -sL https://go2.vip/bisu-file -o ./bisu.bash && sudo chmod 755 ./bisu.bash && sudo ./bisu.bash -f install
+## sudo curl -sL https://go2.vip/bisu-file -o ./bisu && sudo chmod 755 ./bisu && sudo ./bisu -f install
 ## Set the required version of BISU
-export THIS_REQUIRED_BISU_VERSION=">=5.2.0"
-export BISU_PRODUCTION="/usr/local/sbin/bisu.bash"
-export BISU_TESTING="$HOME/Documents/Projects/bisu/bisu/bisu.latest.bash"
-## Decide the reference BISU file by enviroment of production or testing
-export BISU_ENV="production"
-## Import BISU file
-[ "$BISU_ENV" == "production" ] && source "$BISU_PRODUCTION" || [ "$BISU_ENV" == "testing" ] && source "$BISU_TESTING" || {
-    echo -e "Error: Unable to load BISU" >&2
-    exit 1
-}
+export THIS_REQUIRED_BISU_VERSION=">=7.0.0"
+export BISU_PATH="/usr/local/sbin/bisu"
+
+## <required-script-files>
+export REQUIRED_SCRIPT_FILES=()
+## </required-script-files>
+
 ## <required-external-commands>
 export REQUIRED_EXTERNAL_COMMANDS=()
 ## </required-external-commands>
+
 ## <auto-run>
 export AUTORUN=()
 ## </auto-run>
+
 ## <exit-with-commands>
 export EXIT_WITH_COMMANDS=()
 ## </exit-with-commands>
+
+## Import BISU file
+source "$BISU_PATH" || {
+    printf "Error: Unable to load BISU\n" >&2
+    exit 1
+}
 ######################################################## BISU Importer End ###########################################################
