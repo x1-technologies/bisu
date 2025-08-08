625180ba8cdcf714508e38b024bbcc9a91c90bc5
Rick Godfrey <rickgodfreygo@hotmail.com>
2025-08-08 21:03:49 +0800

    updated BISU Importer to 20250808Z1
    Set the column limit to 160 chars;
    Added shellcheck avoidance for interfering removal;
    Added a bisu importing try for the current folder;

--- bisu/bisu.importer.latest.sh
+++ bisu/bisu.importer.latest.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
-######################################################## BISU Importer Start #########################################################
+# shellcheck shell=bash
+# shellcheck disable=SC2071,SC1087,SC2159,SC2070,SC2155,SC2046,SC2206,SC2154,SC2157,SC2128,SC2120,SC2178,SC2086,SC2009,SC2015,SC2004,SC2005,SC1003,SC1091,SC2034
+# shellcheck disable=SC2207,SC2181,SC2018,SC2019,SC2059,SC2317,SC2064,SC2188,SC1090,SC2106,SC2329,SC2235,SC1091,SC2153
+################################################################### BISU Importer Start ########################################################################
 ## Official Web Site: https://bisu.cc
-## Version: 20250726Z1
+## Version: 20250808Z1
 ## Recommended BISU PATH: /usr/local/sbin/bisu
 ## Set the required version of BISU
 export THIS_REQUIRED_BISU_VERSION=">=9.0.0"
+export BISU_BOUND="./bisu"
 export BISU_PATH="/usr/local/sbin/bisu"
 export TERMUX_BISU_PATH="/data/data/com.termux/files/usr/bin/bisu"
 export BISU_DL_COMMAND="curl -sL https://g.bisu.cc/bisu -o ./bisu && chmod +x ./bisu && ./bisu -f install"
@@ -45,7 +49,7 @@ export EXIT_WITH_COMMANDS=()
 ## </exit-with-commands>
 
 ## Import BISU file
-source "$BISU_PATH" 2>/dev/null || source "$TERMUX_BISU_PATH" 2>/dev/null || {
+source "$BISU_BOUND" 2>/dev/null || source "$BISU_PATH" 2>/dev/null || source "$TERMUX_BISU_PATH" 2>/dev/null || {
     command -v "bash" &>/dev/null || {
         printf '%s\n' "BISU is bash dedicated, using it in another shell is risky."
         exit 1
@@ -57,4 +61,4 @@ source "$BISU_PATH" 2>/dev/null || source "$TERMUX_BISU_PATH" 2>/dev/null || {
     printf '%s\n' "Error: Unable to load BISU."
     exit 1
 }
-######################################################## BISU Importer End ###########################################################
+#################################################################### BISU Importer End #########################################################################
