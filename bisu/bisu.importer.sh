f2631f5283159d0b4d150d9314a83ede783a01dc
Rick Godfrey <rickgodfreygo@hotmail.com>
2025-08-18 11:42:55 +0800

    updated BISU Importer to v9-20250818Z1
    updated BISU Importer to v9-20250818Z1

--- bisu/bisu.importer.sh
+++ bisu/bisu.importer.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 # shellcheck shell=bash
 # shellcheck disable=SC2071,SC1087,SC2159,SC2070,SC2155,SC2046,SC2206,SC2154,SC2157,SC2128,SC2120,SC2178,SC2086,SC2009,SC2015,SC2004,SC2005,SC1003,SC1091,SC2034
-# shellcheck disable=SC2207,SC2181,SC2018,SC2019,SC2059,SC2317,SC2064,SC2188,SC1090,SC2106,SC2329,SC2235,SC1091,SC2153,SC2076,SC2102,SC2324,SC2283,SC2179
+# shellcheck disable=SC2207,SC2181,SC2018,SC2019,SC2059,SC2317,SC2064,SC2188,SC1090,SC2106,SC2329,SC2235,SC1091,SC2153,SC2076,SC2102,SC2324,SC2283,SC2179,SC2162
 ################################################################### BISU Importer Start ########################################################################
 ## Official Web Site: https://bisu.cc
-## Version: v9-20250814Z3
-## Recommended BISU PATH: /usr/local/sbin/bisu
+## Version: v9-20250818Z1
+## Recommended BISU PATH: /usr/local/bin/bisu
 ## Set the required version of BISU
 export THIS_REQUIRED_BISU_VERSION=">=9.0.0"
 export BISU_BOUND="./bisu"
-export BISU_PATH="/usr/local/sbin/bisu"
+export BISU_PATH="/usr/local/bin/bisu"
 export TERMUX_BISU_PATH="/data/data/com.termux/files/usr/bin/bisu"
 export BISU_DL_COMMAND="curl -sL https://g.bisu.cc/bisu-file -o ./bisu && chmod +x ./bisu && ./bisu -f install"
 
@@ -18,12 +18,14 @@ export BISU_DL_COMMAND="curl -sL https://g.bisu.cc/bisu-file -o ./bisu && chmod 
 export UTILITY_NAME=""
 # Set this utility's version number
 export UTILITY_VERSION=""
-# Set this utility's doc URI
-export UTILITY_INFO_URI=""
 # Set this utility's last release date
 export LAST_RELEASE_DATE=""
+# Set this utility's doc URI
+export UTILITY_INFO_URI=""
+# Installation target dir
+export TARGET_DIR=""
 # Atomic mutex lock switch for single-threaded utilities
-export ATOMIC_MUTEX_LOCK="true"
+export ATOMIC_MUTEX_LOCK="false"
 # Signature verification switch
 export VERIFY_UTILITY_SIG="false"
 # GPG Signature algorithm set
