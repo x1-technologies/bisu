2d9d20e83beb882f71b3240a498d23baf22035d9
estbsn <rickgodfreygo@hotmail.com>
2025-09-16 02:45:28 +0800

    upgraded BISU Importer to v11-20250915Z1
    upgraded BISU Importer to v11-20250915Z1

--- bisu/bisu.importer.sh
+++ bisu/bisu.importer.sh
@@ -4,63 +4,63 @@
 # shellcheck disable=SC2207,SC2181,SC2018,SC2019,SC2059,SC2317,SC2064,SC2188,SC1090,SC2106,SC2329,SC2235,SC1091,SC2153,SC2076,SC2102,SC2324,SC2283,SC2179,SC2162
 # shellcheck disable=SC2170,SC2219,SC2090,SC2190,SC2145,SC2294,SC2124,SC2139,SC2163,SC2043
 ################################################################### BISU Importer Start ########################################################################
-## Official Web Site: https://bisu.cc
-## Version: v10-20250824Z1
+## Official Web Site: https://bisu.dev
+## Version: v11-20250915Z1
 ## Recommended BISU PATH: /usr/local/bin/bisu
 ## Set the required version of BISU
-export THIS_REQUIRED_BISU_VERSION=">=10.0.0"
+export BISU_VERSION_REQUIREMENT=">=11.0.0"
 export BISU_BOUND="./bisu"
-export BISU_PATH="/usr/local/bin/bisu"
-export TERMUX_BISU_PATH="/data/data/com.termux/files/usr/bin/bisu"
-export BISU_DL_COMMAND="curl -sL https://g.bisu.cc/bisu-file -o ./bisu && chmod +x ./bisu && sudo ./bisu -f install"
+export BISU_BIN_PATH="/usr/local/bin/bisu"
+export BISU_TERMUX_BIN_PATH="/data/data/com.termux/files/usr/bin/bisu"
+export BISU_DL_COMMAND="curl -sL https://go.bisu.dev/bisu-file -o ./bisu && chmod +x ./bisu && sudo ./bisu -f install"
 
 ## <user-customized-variables>
 # Set this utility's name
-export UTILITY_NAME=""
+export BISU_CURRENT_UTIL_NAME=""
 # Set this utility's version number
-export UTILITY_VERSION=""
+export BISU_CURRENT_UTIL_VERSION=""
 # Set this utility's last release date
-export LAST_RELEASE_DATE=""
+export BISU_CURRENT_UTIL_LAST_RELEASE_DATE=""
 # Set this utility's doc URI
-export UTILITY_INFO_URI=""
-# Installation target dir
-export TARGET_DIR=""
+export BISU_CURRENT_UTIL_INFO_URI=""
 # Set this utility's asc sig file URL
-export UTILITY_ASC_FILE_URL=""
-# Atomic mutex lock switch for single-threaded utilities
-export ATOMIC_MUTEX_LOCK="false"
+export BISU_CURRENT_UTIL_ASC_FILE_URL=""
 # Signature verification switch
-export VERIFY_UTILITY_SIG="false"
+export BISU_CURRENT_UTIL_VERIFY_SIG="false"
 # GPG Signature algorithm set
-export GPG_SIG_ALGO="sha256"
+export BISU_CURRENT_UTIL_GPG_SIG_ALGO="sha256"
+# Installation target dir
+export BISU_CURRENT_UTIL_TARGET_DIR=""
+# Atomic mutex lock switch for single-threaded utilities
+export BISU_USE_AML_LOCK="false"
 # Style code injection function
-export FUNC_STYLE_CODE_INJECTION="__style_code_injection"
+export BISU_STYLE_CODE_INJECTION_FUNC="__style_code_injection"
 # Debug Switch
-export DEBUG_MODE="false"
+export BISU_DEBUG_MODE="false"
 ## </user-customized-variables>
 
 ## <required-external-commands>
-export REQUIRED_EXTERNAL_COMMANDS=()
+export BISU_CURRENT_UTIL_REQUIRED_COMMANDS=()
 ## </required-external-commands>
 
-## <required-scripts>
-export REQUIRED_SCRIPTS=()
-## </required-scripts>
-
 ## <actions-read-only>
-export ACTIONS_RO=()
+export BISU_CURRENT_UTIL_ACTIONS_RO=()
 ## </actions-read-only>
 
 ## <auto-run-commands>
-export AUTORUN=()
+export BISU_CURRENT_UTIL_AUTORUN_COMMANDS=()
 ## </auto-run-commands>
 
+## <required-scripts>
+export BISU_CURRENT_UTIL_REQUIRED_SCRIPTS=()
+## </required-scripts>
+
 ## <exit-with-commands>
-export EXIT_WITH_COMMANDS=()
+export BISU_CURRENT_UTIL_EXIT_WITH_COMMANDS=()
 ## </exit-with-commands>
 
 ## Import BISU file
-source "$BISU_BOUND" 2>/dev/null || source "$BISU_PATH" 2>/dev/null || source "$TERMUX_BISU_PATH" 2>/dev/null || {
+source "$BISU_BOUND" 2>/dev/null || source "$BISU_BIN_PATH" 2>/dev/null || source "$BISU_TERMUX_BIN_PATH" 2>/dev/null || {
     command -v "bash" &>/dev/null || {
         printf '%s\n' "Error: BISU is bash dedicated, using it in another shell is risky."
         exit 1
